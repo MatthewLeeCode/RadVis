53bb1e12357481e1bd96591bff03aeab5205a0fc
Matthew Lee <matthewleemattner@gmail.com>
2023-05-29 15:35:32 +1000

    Removed meshio requirement

--- radvis/__init__.py
+++ radvis/__init__.py
@@ -1,6 +1,6 @@
-from .mesh import compute_mesh
+#from .mesh import compute_mesh
 from .image import load_image, RadImage
 from .visualize import RadSlicer, RadSlicerGroup
 from .processing import normalization, noise_reduction, percentile_clipping, add_padding
 
-__all__ = ["compute_mesh", "load_image", "RadSlicer", "RadSlicerGroup", "normalization", "noise_reduction", "percentile_clipping", "RadImage", "add_padding"]
+__all__ = ["load_image", "RadSlicer", "RadSlicerGroup", "normalization", "noise_reduction", "percentile_clipping", "RadImage", "add_padding"]
